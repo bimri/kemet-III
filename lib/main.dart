ed61a24c69f8067cc995a684023c4c136d83e93e
bimri <bimri@outlook.com>
2024-09-20 02:22:39 +0300

    logger added for prod & linux config platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
+import 'package:logger/logger.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await Hive.initFlutter();
 
-  runApp(const Kemet());
+  final logger = Logger();
+
+  if (!Platform.isLinux) {
+    try {
+      await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      );
+      logger.i('Firebase initialized successfully');
+    } catch (e, stackTrace) {
+      logger.e('Failed to initialize Firebase', e, stackTrace);
+      // Log more details about the error
+      logger.e('Error details: $e');
+      logger.e('Stack trace: $stackTrace');
+    }
+  } else {
+    logger.w('Skipping Firebase initialization on Linux');
+  }
+
+  runApp(const ProviderScope(child: KemetApp()));
 }
 
 class Kemet extends StatelessWidget {
