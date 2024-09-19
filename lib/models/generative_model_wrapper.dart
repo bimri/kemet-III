a706589ae239d02ec293e43056732d2164d10879
bimri <bimri@outlook.com>
2024-09-19 17:07:41 +0300

    generative model wrapper module

---
+++ lib/models/generative_model_wrapper.dart
@@ -0,0 +1,16 @@
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+abstract class GenerativeModelWrapper {
+  Future<GenerateContentResponse> generateContent({required List<Content> content});
+}
+
+class GenerativeModelImpl implements GenerativeModelWrapper {
+  final GenerativeModel _model;
+
+  GenerativeModelImpl(this._model);
+
+  @override
+  Future<GenerateContentResponse> generateContent({required List<Content> content}) {
+    return _model.generateContent(content);
+  }
+}
