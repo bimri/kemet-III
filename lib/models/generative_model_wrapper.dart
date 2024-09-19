4163ce41fef16abf4b41cbd7317b5b2436077d4b
bimri <bimri@outlook.com>
2024-09-20 02:23:02 +0300

    tests setup

--- lib/models/generative_model_wrapper.dart
+++ lib/models/generative_model_wrapper.dart
@@ -1,16 +1,18 @@
-import 'package:google_generative_ai/google_generative_ai.dart';
+// import 'package:google_generative_ai/google_generative_ai.dart';
 
-abstract class GenerativeModelWrapper {
-  Future<GenerateContentResponse> generateContent({required List<Content> content});
-}
+// abstract class GenerativeModelWrapper {
+//   Future<GenerateContentResponse> generateContent({required List<Content> content});
+// }
 
-class GenerativeModelImpl implements GenerativeModelWrapper {
-  final GenerativeModel _model;
+// class GenerativeModelImpl implements GenerativeModelWrapper {
+//   final GenerativeModel _model;
 
-  GenerativeModelImpl(this._model);
+//   GenerativeModelImpl(this._model);
 
-  @override
-  Future<GenerateContentResponse> generateContent({required List<Content> content}) {
-    return _model.generateContent(content);
-  }
-}
+//   @override
+//   Future<GenerateContentResponse> generateContent({required List<Content> content}) {
+//     return _model.generateContent(content);
+//   }
+// }
+
+// // for purposes of testing only
