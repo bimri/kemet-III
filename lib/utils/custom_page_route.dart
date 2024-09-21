fc9652fe8bb6b1b7eed80c87a062425eef55dfa3
bimri <bimri@outlook.com>
2024-09-21 23:49:18 +0300

    page animation router

---
+++ lib/utils/custom_page_route.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomPageRoute extends PageRouteBuilder {
+  final Widget Function(BuildContext) builder;
+
+  CustomPageRoute({required this.builder})
+      : super(
+          pageBuilder: (context, animation, secondaryAnimation) =>
+              builder(context),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            var begin = const Offset(1.0, 0.0);
+            var end = Offset.zero;
+            var curve = Curves.easeInOutCubic;
+            var tween =
+                Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+            var offsetAnimation = animation.drive(tween);
+
+            return SlideTransition(
+              position: offsetAnimation,
+              child: FadeTransition(
+                opacity: animation,
+                child: child,
+              ),
+            );
+          },
+        );
+}
